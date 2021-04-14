8662e252b40ec405993788fea2d3325ad024f214
Milyukov Sergey <serrguk@gmail.om>
2021-04-15 00:49:58 +0700

    SpringHello new classes added

---
+++ src/main/java/com/javastart/hellospring/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.javastart.hellospring.controller;
+
+import com.javastart.hellospring.controller.dto.AccountRequestDTO;
+import com.javastart.hellospring.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/hello")
+    public String helloSpring() {
+        return "Hello, Spring!";
+    }
+
+    @PostMapping("/accounts")
+    public Long createAccount(@RequestBody AccountRequestDTO accountRequestDTO) {
+        return accountService.createAccount(accountRequestDTO.getName(),
+                accountRequestDTO.getEmail(), accountRequestDTO.getBill());
+    }
+}
